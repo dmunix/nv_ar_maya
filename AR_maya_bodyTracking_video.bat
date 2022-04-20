5f0ccfac505534ce71276cf88172d6e6b4882a53
LAPTOP-QP382TKM\munix <davide@genies.com>
2022-04-20 16:43:14 -0700

    added compiled apps

--- AR_maya_bodyTracking_video.bat
+++ AR_maya_bodyTracking_video.bat
@@ -1,3 +1,3 @@
 SETLOCAL
 SET PATH=%PATH%;projects\external\opencv\bin;C:/Program Files/Autodesk/maya2019/bin;
-build\Release\AR_maya_bodyTracking.exe  --model_path=models --mode=0 --in_file=videos/sample_rom_low.mp4 --offline_mode=true --writeOutFiles=false --verbose=true
+apps\AR_maya_bodyTracking.exe  --model_path=models --mode=0 --in_file=videos/sample_rom_low.mp4 --offline_mode=true --writeOutFiles=false --verbose=true
